a19c5313e37296d12223cb9168af4201997b525e
ZINOU39003 <zouhiraya.dz12@gmail.com>
2026-04-10 00:55:23 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'mysql-159b2565-zinou.i.aivencloud.com',
   user: process.env.DB_USER || 'avnadmin',
-  password: process.env.DB_PASSWORD || 'AVNS_I5yYt0o7M0oY7g6fWw8',
+  password: process.env.DB_PASSWORD || 'AWS_SSRrxBV-1Puie7v_FAI',
   database: process.env.DB_NAME || 'bader_db',
   port: process.env.DB_PORT || 27815,
   waitForConnections: true,
